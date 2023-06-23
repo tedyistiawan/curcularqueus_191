953c2d69dd620d7d547cac9f483844d8f98ed714
tedyistiawan <tedypnd07@gmail.com>
2023-06-23 11:11:56 +0700

    1

---
+++ curcularqueus/curcularqueus.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+class Queues {
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+	void insert() {
+		int num;
+		cout << "Enter a number :";
+		cin >> num;
+		cout << endl;
+
+		//Cek apakah antrian penuh.
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue pverflow\n";
+			return;
+		}
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada di posisi terakhir array,kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	void remove() {
+		//cek apakah antrian ksosong
+		if (FRONT == -1) {
+			cout << "queue underflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			// jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+
+	void display() {
+		int FRONT_position  = FRONT;
+		int REAR_position = REAR;
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "queue is empty\n";
+			return;
+		}
+
+		cout << "\nElement in the queue are...\n";
+
+		//jika FRONT_position <= REAR_position, literasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR position, literasi dari FRONT hingga akhir array
+			while (FRONT_position <= n - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+
+			}
+			FRONT_position = 0;
+
+			//literasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};
+
+int main() {
+	Queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+		}
+	}
+}
